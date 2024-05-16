686478f7ce1adb936cbe208520ee4d93dfd6c2f3
codinghubindia <manjun2412@gmail.com>
2024-05-16 18:18:41 +0530

    Updated Design

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
-const restore = document.querySelector('.content').innerHTML;
+const restore = document.querySelector('.form-group').innerHTML;
 
 function calculateAge() {
     const bdat = document.getElementById('dob').value;
     console.log(bdat);
     if(bdat == null || bdat == "") {
-        alert("Enter Date of Birth!")    
-    }
-    else {
-
+        alert("Enter Date of Birth!");    
+    } else {
         const bda = bdat.replaceAll("-", "");
         const by = parseInt(bda.slice(0, 4), 10);
         const bm = parseInt(bda.slice(4, 6), 10);
@@ -23,18 +21,26 @@ function calculateAge() {
             age--;
         }
     
-        const contentElement = document.querySelector('.content');
+        const contentElement = document.querySelector('.form-group');
         contentElement.innerHTML = `
             <div class="inp">
                 <label for="dob">Congrats! You are ${age} years old</label>
             </div>
             <div class="inp Submit">
-                <button onclick="restoreContent()">Restore</button>
+                <button class="restore-btn" onclick="restoreContent()">Restore</button>
             </div>`;
+        
+        // Hide the submit button
+        const btn = document.querySelector('.submit-btn');
+        btn.classList.add('hide');
     }
 }
 
 function restoreContent() {
-    const contentElement = document.querySelector('.content');
+    const contentElement = document.querySelector('.form-group');
     contentElement.innerHTML = restore;
+    
+    // Show the submit button again
+    const btn = document.querySelector('.submit-btn');
+    btn.classList.remove('hide');
 }
